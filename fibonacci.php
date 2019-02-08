308b84b2312c846c26c5c3af64eccec9fa048a5b
ULOKO JOHN IGBO <jay-i@users.noreply.github.com>
2019-02-08 23:58:46 +0100

    Add files via upload

---
+++ fibonacci.php
@@ -0,0 +1,42 @@
+<?php
+if(isset($_POST['submit'])){
+	$n = $_POST['num'];
+	
+	define('NUM', $n);
+	$a = 0;
+	$b = 1;
+	echo "$a $b "; // 0 1
+	
+	for($i = 1; $i <= NUM - 2; $a=$b, $b=$c, $i++){
+		echo $c = $a+$b;
+		echo " ";
+	}
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Fibonacci Series </title>
+</head>
+<body>
+<table>
+<h3>Program to calculate Fibonacci Series</h3>
+<form name="fibonacci" method="post">
+<tr>
+<td colspan="2">Enter number upto you want to print the series.
+</tr>
+<tr>
+<td>Enter Number :</td>
+<td><input type="text" name="num" required></td>
+</tr>
+<tr>
+<td></td>
+<td><input type="submit" value="Check" name="submit" /></td>
+</tr>
+</form>
+</table>
+</body>
+</html>
